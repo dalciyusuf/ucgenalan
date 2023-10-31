2ac2c274df24a55f54e60d3ac38441f38fc753e0
Yusuf <115079732+dalciyusuf@users.noreply.github.com>
2023-10-31 15:51:10 +0300

    Add files via upload

---
+++ ucgenalan.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int main() {
+	int a,b,c;
+	float u,alan;
+	
+	printf("ucgenin kenarlarini sirasi ile giriniz:	");
+	scanf("%d %d %d",&a,&b,&c);
+	u= (a+b+c)/2;
+	
+	alan = sqrt(u*(u-a)*(u-b)*(u-c));
+	printf("alan= %f",alan);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+
+}
